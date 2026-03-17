9cfc9d76d57611dba75883ef94124b11d2e09c06
Vinay <vd3711211@gmail.com>
2026-03-17 22:34:06 +0530

    10

--- for loops - 10/practise.py
+++ for loops - 10/practise.py
@@ -1,27 +1,27 @@
-# #1. Print Numbers 1 to 20
-# for i in range(1,21):
-#     print(i)
+# 1. Print Numbers 1 to 20
+for i in range(1, 21):
+    print(i)
 
-#2.Print Even Numbers
-# for i in range(2,21,2):
-#     print(i)
+# 2.Print Even Numbers
+for i in range(2, 21, 2):
+    print(i)
 
-#3.Print Odd Numbers
-# for i in range(1,20,2):
-#     print(i)
+# 3.Print Odd Numbers
+for i in range(1, 20, 2):
+    print(i)
 
-#4.Sum of Numbers
-# total = 0
-# for i in range(1,11):
-#     total = total + i
-#     print(total)
+# 4.Sum of Numbers
+total = 0
+for i in range(1, 11):
+    total = total + i
+    print(total)
 
-#5.Loop Through List
-# l = ["Apple","Banana","Orange"]
-# for frits in l:
-#     print(frits)
+# 5.Loop Through List
+l = ["Apple", "Banana", "Orange"]
+for frits in l:
+    print(frits)
 
-#6.Count Letters in String
+# 6.Count Letters in String
 name = "Vinay"
 count = 0
 for letter in name:
