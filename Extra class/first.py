097dfc8e5dc37bd582101f3c41f915192b78444f
Vinay <vd3711211@gmail.com>
2026-03-20 22:11:18 +0530

    Extra class

---
+++ Extra class/first.py
@@ -0,0 +1,9 @@
+#using debugger
+i= 0
+while i <= 5:
+    print(i,end=" ")
+    i = i + 1
+
+for i in range(1,3):
+    for j in range(1,11):
+        print(f"{i}X{j}={i*j}")
---
+++ Extra class/practise.py
@@ -0,0 +1,20 @@
+x = 5
+y = 10
+z = x + y
+print(z)
+
+def add(a, b):
+    result = a + b
+    return result
+
+num1 = 3
+num2 = 7
+
+total = add(num1, num2)
+print("Total:", total)
+
+for i in range(1,11):
+    print(i)
+
+l = ["Vinay","Manu","Darshh","Kartik"]
+print(l[0:3])
