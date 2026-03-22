91fe52179ac15d35d1722460631f63dd013b57bf
Vinay <vd3711211@gmail.com>
2026-03-22 12:21:41 +0530

    Practise

--- functions,parameters - 12/practise.py
+++ functions,parameters - 12/practise.py
@@ -1,7 +1,37 @@
-def func(num):
-    if num % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
-    
-print(func(20))
+# Cheak even or odd
+# def add(num):
+#     if num % 2 == 0:
+#         return "Even"
+#     else:
+#         return "odd"
+
+# result = add(4)
+# print(result)
+
+#Sum of 3 numbers
+# def num(a,b,c):
+#     return a+b+c
+
+# output = num(2,4,6)
+# print(output)
+
+#Largest number
+# def large(a,b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+# largest_num = large(5,10)
+# print(f"Largest number is: {largest_num}")
+
+# def table(num):
+#     for i in range(1,11):
+#         print(f"{num}X{i}={num*i}")
+
+# table(5)
+
+#String repeat
+def rep(s):
+    return s * 5
+
+print(rep("Python"))
